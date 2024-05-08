af5768d28a0bc0cf276b4ab5859d75220f7015ad
PhantomGTs <118103192+PhantomGTs@users.noreply.github.com>
2024-05-08 15:23:28 +0300

    Correct

--- prac_task1.1/Program.cs
+++ prac_task1.1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Convert.ToDouble(Console.ReadLine());
+        
+        //Создаем экземпляр стуктуры 
+        Quadro quad = new Quadro(a,b,c);
+        Console.WriteLine(quad.Root());
     }
 }
